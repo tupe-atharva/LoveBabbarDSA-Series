32ef95ddb9b3dbc7f6e19617d1fcf77c6f2c9a90
tupe-atharva <atharva.tupe007@gmail.com>
2025-01-20 09:57:41 +0530

    25 patterns done - final pattern remaining

--- patterns.cpp
+++ patterns.cpp
@@ -4,6 +4,10 @@ int main(){
 
 int n = 4;
 
+
+// ---------------------------------------------------------------------------
+
+
 // PATTERN 1 : ****
 //             ****
 //             ****
@@ -19,6 +23,10 @@ int n = 4;
    
 // }
 
+
+// ---------------------------------------------------------------------------
+
+
 // PATTERN 2 : 1 1 1
 //             2 2 2
 //             3 3 3
@@ -31,6 +39,10 @@ int n = 4;
 //    }
 //    cout<<endl;
 // }
+
+
+// ---------------------------------------------------------------------------
+
 
 
 // PATTERN 3 : 1 2 3 4
@@ -47,6 +59,10 @@ int n = 4;
 //    cout<<endl;
 // }
 
+
+// ---------------------------------------------------------------------------
+
+
 // PATTERN 4 : 3 2 1
 //             3 2 1
 //             3 2 1
@@ -60,6 +76,8 @@ int n = 4;
 //    cout<<endl;
 // }
 
+
+// ---------------------------------------------------------------------------
 
 
 // PATTERN 5 : 1 2 3
@@ -80,6 +98,10 @@ int n = 4;
 // }
 
 
+// ---------------------------------------------------------------------------
+
+
+
 // PATTERN 6 : *
 //             * *
 //             * * *
@@ -95,6 +117,11 @@ int n = 4;
     
 
 // }
+
+
+// ---------------------------------------------------------------------------
+
+
 
 // PATTERN 7 : 1
 //             2 2
@@ -114,6 +141,9 @@ int n = 4;
 // }
 
 
+// ---------------------------------------------------------------------------
+
+
 // PATTERN 8 : 1
 //             2 3 
 //             4 5 6
@@ -130,6 +160,10 @@ int n = 4;
   
 //    cout<<endl;
 // }
+
+
+// ---------------------------------------------------------------------------
+
 
 // PATTERN 9 : 1
 //             2 3 
@@ -150,6 +184,10 @@ int n = 4;
 //     cout<<endl;
 // }
 
+
+// ---------------------------------------------------------------------------
+
+
 // PATTERN 10 : 1
 //              2 1
 //              3 2 1 
@@ -164,6 +202,10 @@ int n = 4;
 //     }
 //     cout<<endl;
 // }
+
+
+// ---------------------------------------------------------------------------
+
 
 
 // PATTERN 11 : A A A
@@ -183,6 +225,11 @@ int n = 4;
 // }
 
 
+
+// ---------------------------------------------------------------------------
+
+
+
 // PATTERN 12 : A B C
 //              A B C 
 //              A B C
@@ -197,6 +244,10 @@ int n = 4;
 //    }
 //    cout<<endl;
 // }
+
+
+// ---------------------------------------------------------------------------
+
 
 // PATTERN 13 : A B C
 //              D E F 
@@ -213,6 +264,9 @@ int n = 4;
 //    }
 //    cout<<endl;
 // }
+
+
+// ---------------------------------------------------------------------------
 
 
 // PATTERN 14 : A B C 
@@ -232,9 +286,13 @@ int n = 4;
 // }
 
 
+
+// ---------------------------------------------------------------------------
+
+
 // PATTERN 15 : A
-//           B B
-//           C C C
+//              B B
+//              C C C
 
 // char ch = 'A';
 // for (int i = 1; i <=3; i++)
@@ -246,6 +304,327 @@ int n = 4;
 //    cout<<endl;
 //    ch++;
 // }
+
+
+// ---------------------------------------------------------------------------
+
+// PATTERN 16 : A
+//              B C
+//              D E F
+//              G H I J
+
+
+// char ch = 'A';
+// for (int i = 1; i <=4; i++)
+// {
+//    for(int j = 1; j <=i; j++)
+//    {
+//         cout<<ch<<" "; 
+//          ch++;
+//          // simply after printing the character, increment it for next iterations.
+//          // j<=i ensures the patterns triangular shape
+//         //  as the inner loop runs only according to the value of i
+//    }
+//    cout<<endl;
+  
+// }
+
+
+// ---------------------------------------------------------------------------
+
+// PATTERN 17 : A
+//              B C 
+//              C D E
+//              D E F G 
+
+// for (int i = 1; i <=4; i++)
+// {
+//     char ch = 65 + i-1;
+//     //here 65 is the ASCII value for 'A'.
+//   for (int j = 1; j <=i; j++)
+//   {
+//     cout<<ch<<" ";
+//     ch++;
+//   }
+//   cout<<endl;
+// }
+
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 18 : D
+//              C D 
+//              B C D
+//              A B C D
+
+
+// char ch = 'A';
+
+// for (int i = 1; i <=n; i++)
+// {
+//     char start = ch + n - i;
+//     //this is the starting char of each row.
+//     //now that we have the starting char for each row, just increment it every time the inner loop works.
+//    for (int j = 1; j <=i; j++)
+//    {
+//      cout<<start<<" ";
+//      start++;
+//    }
+//    cout<<endl;
+// }
+
+
+// ---------------------------------------------------------------------------
+
+// PATTERN 19 :        *
+//                   * *
+//                 * * *
+//               * * * *
+
+// for (int i = 1; i <=n; i++)
+// {
+//     for (int k = 0; k<n-i; k++)
+//     {
+//         cout<<" "; 
+//     }
+//     //this k loop prints spaces witht the equation k<n-i. 
+    
+//    for (int j = 1; j<=i; j++)
+//    {
+//     cout<<"*";
+//    }
+//    cout<<endl;
+// }
+
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 20 : * * * *
+//              * * *
+//              * *
+//              * 
+
+
+// for (int i = 0; i <=n; i++)
+// {
+    
+//     for (int j = 0; j<n-i; j++)
+//     {
+//        cout<<"*"<<" ";
+//     }
+//     cout<<endl;
+
+
+//     // for (int k = 4; k < n+k; k--)
+//     // {
+//     //    cout<<" ";
+//     // }
+//     // there is no need to print the spaces man !
+//     // simply print the stars in decreasing order
+
+
+// }
+
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 21 : * * * * 
+//                * * *
+//                  * *
+//                    * 
+
+//now here you need to print the spaces too and then print stars.
+
+
+// for (int i = 0; i <=n; i++)
+// {
+//    for (int k = 0; k < i; k++)
+//    {
+//     cout<<" ";
+//    }
+
+//    for (int j = 0; j< n-i; j++) // consider the fact that for every row the number of stars is equal to n-i ie. for row 0 --> 4-0, for row 1 --> 4-1, for row 2 --> 4-2 and for row 3 --> 4-3. 
+//    {
+//     cout<<"*";
+//    }
+//    cout<<endl;
+   
+// }
+
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 22 : 1 1 1 1
+//                2 2 2
+//                  3 3 
+//                    4
+
+//use the same logic as above but instead of printing stars, every row should print the row number.
+
+// for (int i = 0; i <=n; i++)
+// {
+//    for (int k = 0; k < i; k++)
+//    {
+//     cout<<" ";
+//    }
+
+//    for (int j = 0; j< n-i; j++) // consider the fact that for every row the number of stars is equal to n-i ie. for row 0 --> 4-0, for row 1 --> 4-1, for row 2 --> 4-2 and for row 3 --> 4-3. 
+//    {
+//     cout<<i+1;
+//    }
+//    cout<<endl;
+// }
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 23 :      1
+//                 2 2
+//               3 3 3
+//             4 4 4 4 
+
+//APPROACH : print spaces first (for each row print n-i spaces) - use two   inner loops, one for spaces, one for number printing, then print the row number.
+
+// for (int i = 1; i <=n; i++)
+// {
+//     for (int k = 1; k<=n-i; k++)
+//     {
+//         cout<<" ";
+//     }
+
+//     for (int j = 1; j <=i; j++)
+//     {
+//         cout<<i;
+//     }
+//     cout<<endl;
+    
+    
+// }
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 24 : 1 2 3 4
+//                2 3 4 
+//                  3 4
+//                    4
+
+
+// APPROACH : print spaces using a loop first (like above pattern), then print the value of j ie. the column number.
+
+// for (int i = 1; i <=n; i++)
+// {
+//     for (int k = 1; k<i; k++)
+//     {
+//         cout<<" ";
+//     }
+
+//obsreve that in every row the numbers start from the value of i, ie for row 2, numbers start from 2 ...
+//     for (int j = i; j<=n ; j++)
+//     {
+//         cout<<j;
+//     }
+//     cout<<endl;  
+// }
+
+// ---------------------------------------------------------------------------
+
+
+// PATTERN 25 :   
+
+//          _ _ _ 1
+//          _ _ 1 2 1
+//          _ 1 2 3 2 1
+//          1 2 3 4 3 2 1
+
+
+//APPROACH : let's try to print this pattern in stars first then move to numbers.
+
+// for (int i = 1; i <=n; i++)
+// {
+
+//     //print spaces wala triangle
+//    for (int k = 1; k<=n-i; k++)
+//     {
+//         cout<<" ";
+//     }
+   
+
+//    //print 2nd triangle
+//    for (int j = 1; j <=i; j++)
+//    {
+//    cout<<j;
+//    }
+  
+
+
+//    // print 3rd triangle
+
+//     int value = i-1;
+//    for (int k = 0; k<i-1; k++)
+//    {
+//     // int value = i-1; (if this equation is here then for every iteratino value of variable value will be re-initialized to value of i, so instead declare this outside the loop)
+//     //  cout<<"*";
+//     //tried printing 3rd triangle with stars first 
+//     // now lets replace stars with numbers.
+//     // 1
+//     // 2 1
+//     // 3 2 1
+//     // we can see that the numbers start from i and decrease.
+
+//     cout<<value;
+//     value--;
+//    }
+   
+//     cout<<endl;
+// }  
+// ---------------------------------------------------------------------------
+
+
+// HW PATTERN :    
+
+//      1 2 3 4 5 5 4 3 2 1
+//      1 2 3 4 * * 4 3 2 1
+//      1 2 3 * * * * 3 2 1
+//      1 2 * * * * * * 2 1
+//      1 * * * * * * * * 1
+
+
+// APPROACH : treat this pattern as three seperate triangles.
+
+
+for (int i = 1; i <=5; i++)
+{
+    
+    // triangle 1 
+
+    for (int j = 1; j < ; j++)
+    {
+        /* code */
+    }
+    
+
+
+
+
+
+    // triangle 2 
+
+
+
+
+
+
+
+    // triangle 3 
+}
+
+
+// ---------------------------------------------------------------------------
 
 
 return 0;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,1 @@
-for (int i = 1; i <=4; i++)
-// {
-//    int value = i;
+cout<<endl;  
