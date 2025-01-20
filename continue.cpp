ca8a6257e8e0fcee8b1e72179f84d1fcca360bf3
tupe-atharva <atharva.tupe007@gmail.com>
2025-01-20 11:37:20 +0530

    use of continue keyword

---
+++ continue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+for (int i = 0; i < 5; i++)
+{
+    cout<<"HI"<<endl;
+      cout<<"HELLO"<<endl;
+      continue;
+        cout<<"REPLY TOH KARDE"<<endl; //this statement will never be printed
+}
+
+return 0;
+}
