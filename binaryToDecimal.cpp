db073ff0af86bef73ebe36eb97088b22dd787511
tupe-atharva <atharva.tupe007@gmail.com>
2025-01-21 13:19:20 +0530

    ..

---
+++ binaryToDecimal.cpp
@@ -0,0 +1,7 @@
+//WAP To convert entered binary number to decimal number
+
+#include<iostream>
+using namespace std;
+int main(){
+return 0;
+}
---
+++ squareRoot.cpp
@@ -0,0 +1,7 @@
+//WAP to calculate square root of n (no need to calculate the decimal part)
+
+#include<iostream>
+using namespace std;
+int main(){
+return 0;
+}
