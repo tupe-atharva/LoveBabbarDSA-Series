68ea6170063c0b593fc00126cb2e5249c28de1b7
tupe-atharva <atharva.tupe007@gmail.com>
2025-01-30 12:37:08 +0530

    optimized linear search function.

--- DSA Learning/linearSearchInArray.cpp
+++ DSA Learning/linearSearchInArray.cpp
@@ -5,16 +5,26 @@
 using namespace std;
 
 bool search (int arr[] ,int num, int size){
-    bool search = false;
+    //bool search = false;
     for (int i = 0; i < size; i++)
     {
         if (arr[i] == num)
         {
-            search = true;
+            return 1;
+            //search = true;
+            break; // exits the loop when target elemnet is found.
         }
+        
     }
-     return search;
+    return 0;
+    //return search;
 }
+// Optimized the searching function by adding a break statement and by removing the boolean variable search. (simply return 1 for present and 0 for absent)
+
+
+
+
+
 int main(){
 
     int array[100];
@@ -37,10 +47,10 @@ int main(){
     // ✅ search(array, n, size); → Correct (passing the whole array)
     if (ans)
     {
-        cout<< "The entered element is present  : " <<endl;
+        cout<< "The entered element is present " <<endl;
     }
     else{
-        cout<< "The entered element is NOT present  : " <<endl;
+        cout<< "The entered element is NOT present  " <<endl;
     }
     
 
