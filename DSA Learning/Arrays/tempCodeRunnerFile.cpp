e820e93baaf004e72e17d11e85c4119a0f37d8f4
tupe-atharva <atharva.tupe007@gmail.com>
2025-02-03 16:51:34 +0530

    .

--- DSA Learning/Arrays/tempCodeRunnerFile.cpp
+++ DSA Learning/Arrays/tempCodeRunnerFile.cpp
@@ -1,3 +1,7 @@
- // else{
-        //     cout<<"out of bound " << endl;
-        // }
+for (int i = 0 ; i<4 ; i++)
+        {
+            if (arr [i] > arr [i+1])
+            {
+                swap (arr[i] , arr[i+1]);
+            }
+        }
