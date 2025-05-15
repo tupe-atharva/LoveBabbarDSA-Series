41e08b5a39c1dd7efc8a1f49ed2f4f50663812ba
tupe-atharva <atharva.tupe007@gmail.com>
2025-05-15 13:41:07 +0530

    Created a function to calculate the 'n'th Fibonacci number using recursion and using looping.

---
+++ DSA Learning/Recursion/fibonacciUsingRecursion.cpp
@@ -0,0 +1,55 @@
+// #include<iostream>
+// using namespace std;
+
+// int fibonacci (int n ){
+//   int series[n];
+//   series[0] = 0;
+//   series[1] = 1;
+//   int i = 2;
+//   // Base case - n == 8
+//   if (n == 8)
+//   {
+//       return 0;
+//   }
+
+//   // Processing
+//   series[i] = series[i-1] + series[i-2];
+    
+
+//   // Recursive relation : f(n) = f(n-1) + f(n-2)
+//   return fibonacci (i+1);
+    
+// 
+// int main(){ 
+    
+//     int n = 8;
+//     cout << "The fibonacci series upto 8th element is : " <<     fibonacci(n) << endl;
+
+//     return 0;
+// }
+
+
+
+#include<iostream>
+using namespace std;
+
+int fib(int n) {
+    //vector<int> series[n];
+    int* series = new int[n];
+    series[0] = 0;
+    series[1] = 1;
+    int ans;
+
+    for (int i = 2 ; i <= n ; i++){
+        series[i] = series[i-1] + series[i-2];
+        ans = series[i];
+    }
+    return ans;
+}
+
+int main(){ 
+ 
+    cout << "The 4th element is : " << fib(4) << endl;
+ 
+    return 0;
+}
