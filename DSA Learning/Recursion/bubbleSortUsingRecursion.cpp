c71c11688eeb5c8ce6fc48ea3531f49dbd5c2c3d
tupe-atharva <atharva.tupe007@gmail.com>
2025-05-22 22:21:06 +0530

    .

--- DSA Learning/Recursion/bubbleSortUsingRecursion.cpp
+++ DSA Learning/Recursion/bubbleSortUsingRecursion.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
 using namespace std;
 
+void bubbleSort (int *arr , int length){
+    int i = 0;
+    // Base Case : 
+    if(i == length - 1)
+        //return *arr;
 
+
+    // Processing : 
+    if (arr[i] > arr[i+1])
+        swap(arr[i] , arr[i+1]);
+
+    // Recursive Call : 
+
+
+}
 
 
 
 int main(){ 
  
- 
+    int arr[5] = { 23, 45, 22, 54, 12};
+    int length = 5;
+    bubbleSort (arr , length);
  
     return 0;
 }
