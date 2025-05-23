eacb05c17bbcecef64113e7bfe876b692142f505
tupe-atharva <atharva.tupe007@gmail.com>
2025-05-23 16:17:17 +0530

    Created a recurisve function for sorting using buuble sort.

--- DSA Learning/Recursion/bubbleSortUsingRecursion.cpp
+++ DSA Learning/Recursion/bubbleSortUsingRecursion.cpp
@@ -1,19 +1,35 @@
 #include<iostream>
 using namespace std;
 
+
+// void print (int *arr , int length){
+//    for (int i = 0; i < length; i++)
+//     {
+//        cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+
 void bubbleSort (int *arr , int length){
-    int i = 0;
+    
+    
     // Base Case : 
-    if(i == length - 1)
-        //return *arr;
+    if(length == 0 || length == 1) // ie. if only 1 or 0 elements present then array is already sorted.
+        return ;
 
 
     // Processing : 
-    if (arr[i] > arr[i+1])
-        swap(arr[i] , arr[i+1]);
-
+    for (int i = 0; i < length - 1; i++)
+    {
+       if (arr[i] > arr[i+1])
+            swap(arr[i] , arr[i+1]);
+        // This loop will send the greatest element of the 1st iteration in the last index
+        // Recursive call will work on the next elements in line.
+    }
+    
     // Recursive Call : 
-
+    return (bubbleSort(arr , length - 1));
 
 }
 
@@ -24,6 +40,13 @@ int main(){
     int arr[5] = { 23, 45, 22, 54, 12};
     int length = 5;
     bubbleSort (arr , length);
+    
+    for (int i = 0; i < length; i++)
+    {
+       cout << arr[i] << " ";
+    }
+    cout << endl;
+    
  
     return 0;
 }
