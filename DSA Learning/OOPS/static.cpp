c32e8e5b75200abb156fa215c9f35e47eeb1f939
tupe-atharva <atharva.tupe007@gmail.com>
2025-05-28 16:55:27 +0530

    Created a static function which initializes the static data member

--- DSA Learning/OOPS/static.cpp
+++ DSA Learning/OOPS/static.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 class hero {
     public :
-        static int timeToComplete;
+        static int timeToComplete;  
+        static int demo(){
+            return timeToComplete;
+        }
 };
 
 int hero :: timeToComplete = 45;
 
 int main(){ 
- 
+    //cout << hero::demo(45) << endl;
     cout << hero::timeToComplete << endl;
  
     return 0;
