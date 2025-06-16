8011076199563de4e402879c06d81b17671ee2ef
tupe-atharva <atharva.tupe007@gmail.com>
2025-06-16 12:26:42 +0530

    Created a function to reverse a Singly Linked List.

--- DSA Learning/Linked List/reverseLL.cpp
+++ DSA Learning/Linked List/reverseLL.cpp
@@ -16,6 +16,19 @@ class node{
         }
 };
 
+void insertNode(node* &head , int position , int value){
+    node* temp = head;
+    int count = 1;
+    while(count < position - 1){
+        temp = temp -> next;
+        count ++;
+    }
+    node *nodeToInsert = new node(value);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+}
+
 void print(node* &head){
     node *temp = head;
     while(temp != NULL){
@@ -25,11 +38,33 @@ void print(node* &head){
     cout << endl;
 }
 
+void reverseLL(node* &head){
+    node *curr= head;
+    node *prev = NULL;
+    node *forward = NULL;
+    
+    while(curr != NULL){
+        
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    head = prev; // as in the end the prev pointer points to head of the reversed LL.
+}
+
 int main(){ 
  
     node *node1 = new node(10);
     node *head = node1;
+    insertNode(head , 2 , 20);
+    insertNode(head , 3 , 30);
+    insertNode(head , 4 , 40);
+    insertNode(head , 5 , 50);
     print(head);
- 
+    
+    reverseLL (head);
+    print(head);
+
     return 0;
 }
