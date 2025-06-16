b0689c783689ce6268d813622e516f0aee49d2d3
tupe-atharva <atharva.tupe007@gmail.com>
2025-06-16 14:27:39 +0530

    Created an function that returns the middle element of a Singly Linked List.

---
+++ DSA Learning/Linked List/findMiddle.cpp
@@ -0,0 +1,80 @@
+// WAP to find the middle of a given LL.
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+class node{
+    public : 
+        int data;
+        node *next;
+
+        node(int data){
+            this -> data = data;
+            this -> next = NULL;
+        }
+};
+
+void insertNode(node* &head , int position , int value){
+    node* temp = head;
+    int count = 1;
+    while(count < position - 1){
+        temp = temp -> next;
+        count ++;
+    }
+    node *nodeToInsert = new node(value);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+}
+
+void print(node* &head){
+    node *temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " " ;
+        temp = temp -> next;
+    } 
+    cout << endl;
+}
+
+int findMiddle(node* &head){
+
+    int length = 0;
+    node *temp = head;
+    while (temp != NULL){
+        temp = temp -> next;
+        length++;
+    }
+    //cout << "Length : " <<length << endl;
+    int position;
+    if(length%2 == 0){
+        // ie.even.
+        position = (length/2) + 1;
+    }
+    else{
+        position = (length/2) + 1;
+    }
+    //return position;
+    node *ans = head;
+    int count = 1;
+    while(count < position){
+        ans = ans -> next;
+        count ++;
+    }
+    return ans -> data;
+}
+
+int main(){ 
+ 
+    node *node1 = new node(10);
+    node *head = node1;
+    insertNode(head , 2 , 20);
+    insertNode(head , 3 , 30);
+    insertNode(head , 4 , 40);
+    insertNode(head , 5 , 50);
+    insertNode(head , 5 , 60);
+    print(head);
+
+    cout << "The Middle element is : " << findMiddle(head) << endl;
+    return 0;
+}
