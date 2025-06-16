2b1481acebd2d674550d7a092e7cb649e2d95d9a
tupe-atharva <atharva.tupe007@gmail.com>
2025-06-16 15:00:56 +0530

    Optimized solutioin to find the middle of a Singly Linked list.

---
+++ DSA Learning/Linked List/findMiddle2.cpp
@@ -0,0 +1,86 @@
+// WAP to find the middle of a given LL.
+// OPTIMIZED APPROACH.
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+class node{
+    public : 
+        int data;
+        node *next;
+
+        node(int data){
+            this -> data = data;
+            this -> next = NULL;
+        }
+};
+
+void insertNode(node* &head , int position , int value){
+    node* temp = head;
+    int count = 1;
+    while(count < position - 1){
+        temp = temp -> next;
+        count ++;
+    }
+    node *nodeToInsert = new node(value);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+}
+
+void print(node* &head){
+    node *temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " " ;
+        temp = temp -> next;
+    } 
+    cout << endl;
+}
+
+int getLength(node* &head){
+    int length = 0;
+    node *temp = head;
+    while (temp != NULL){
+        temp = temp -> next;
+        length++;
+    }
+    return length;
+}
+
+int findMiddle(node* &head){
+
+    // if(head == NULL){
+    //     return NULL;
+    // }
+    if(head -> next == NULL){
+        return head -> data;
+    }
+
+    node *pro = head -> next;
+    node *noob = head;
+
+    while(pro != NULL){
+        pro = pro -> next;
+        if(pro != NULL){ // this is an imp condition, check if pro is NULL, if yes then it cant move any forward.
+            pro = pro -> next;
+        }
+        noob = noob -> next;
+    }
+    return noob -> data;
+}
+
+int main(){ 
+ 
+    node *node1 = new node(10);
+    node *head = node1;
+    insertNode(head , 2 , 20);
+    insertNode(head , 3 , 30);
+    insertNode(head , 4 , 40);
+    insertNode(head , 5 , 50);
+    //insertNode(head , 5 , 60);
+    print(head);
+
+    cout << "The Middle element is : " << findMiddle(head) << endl;
+    return 0;
+}
